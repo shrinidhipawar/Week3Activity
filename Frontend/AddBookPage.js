a90507ccc1305acef031143788673d2d4b71731e
SouravKKR <souravprogramming@gmail.com>
2024-12-31 19:06:44 +0530

    Done ig

---
+++ Frontend/AddBookPage.js
@@ -0,0 +1,21 @@
+import { sendRequest } from "./SendRequest.js";
+const bookNameInput = document.querySelector("#book-name");
+const bookQuantityInput = document.querySelector("#book-quantity");
+const addBookButton = document.querySelector("#add-book-button");
+
+addBookButton.addEventListener("click", () =>
+{
+    const bookName = bookNameInput.value;
+    const bookQuantity = bookQuantityInput.value;
+
+    if (bookId && bookName && bookQuantity)
+    {
+        const book = { bookName: bookName, quantity: bookQuantity };
+        sendRequest("INSERT", book);
+    }
+    else
+    {
+        alert("Please fill all the fields");
+    }
+});
+
