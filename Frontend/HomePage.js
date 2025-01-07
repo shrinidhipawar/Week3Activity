e459bf94be34282e2dcea5c5a841d01ee376a2f5
SouravKKR <souravprogramming@gmail.com>
2025-01-07 19:37:54 +0530

    SMALL CNANGE

--- Frontend/HomePage.js
+++ Frontend/HomePage.js
@@ -1,5 +1,6 @@
 const addBooksButton = document.querySelector(".add-book-button");
 const borrowReturnButton = document.querySelector(".borrow-return-button");
+const borrowListButton = document.querySelector(".borrow-list-button");
 
 addBooksButton.addEventListener("click", () => 
 {
@@ -11,4 +12,7 @@ borrowReturnButton.addEventListener("click", () =>
     window.location.href = "BorrowReturnPage.html";
 });
 
-
+borrowListButton.addEventListener("click", () =>
+{
+    window.location.href = "BorrowListPage.html";
+});
